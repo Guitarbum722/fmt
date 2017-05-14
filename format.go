1db6f5b7eb41c44c75dc23e5b91862949ac4d0bd
guitarbum722 <johnkenneth.moore@gmail.com>
2017-05-14 15:18:54 -0700

    Add Printfln function to print a formatted string with an appended new line

---
+++ format.go
@@ -0,0 +1,12 @@
+package fmt
+
+import (
+	"fmt"
+	"os"
+)
+
+// Printfln works exactly the same as the standard lib's Printf, but a new line is appended automatically
+func Printfln(format string, a ...interface{}) (n int, err error) {
+	format += "\n"
+	return fmt.Fprintf(os.Stdout, format, a...)
+}
