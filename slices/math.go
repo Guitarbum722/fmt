c932db36f1bd784acb60154dfaba1bd9463b6e3a
guitarbum722 <johnkenneth.moore@gmail.com>
2017-05-16 19:19:59 -0700

    change function signature since the parameter are of the same type

--- slices/math.go
+++ slices/math.go
@@ -5,7 +5,7 @@ const testVersion = 1
 // AddUint8s adds each element of first to the respective element of second and returns a new slice holding the result.
 // If the length of the inputs are different, the result will be the length of the shorter input.
 // The resulting indexes
-func AddUint8s(first []uint8, second []uint8) []uint8 {
+func AddUint8s(first, second []uint8) []uint8 {
 	var result []uint8
 
 	switch len(first) <= len(second) {
