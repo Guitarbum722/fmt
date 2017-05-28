69a82c46222e37ac73a8f82240404c9df8a4201f
guitarbum722 <johnkenneth.moore@gmail.com>
2017-05-27 22:19:54 -0700

    add IsPrime function

---
+++ math.go
@@ -0,0 +1,14 @@
+package box
+
+// Is prime returns true if input n is a prime number
+func IsPrime(n int) bool {
+	if n%2 == 0 {
+		return false
+	}
+	for i := 3; i*i <= n; i += 2 {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
